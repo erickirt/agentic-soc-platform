9e9d0418ed37a6648562504e2b3798effa89c625
rookit <yu5890681@gmail.com>
2026-05-26 14:52:06 +0800

    update PLUGINS

--- PLUGINS/Mock/SIEM/CONFIG.example.py
+++ PLUGINS/Mock/SIEM/CONFIG.example.py
@@ -1,10 +1,5 @@
 # ELK config
 ELK_ENABLED = True
-ELK_HOST = "https://10.10.10.10:9200"
-ELK_USER = "elastic"
-ELK_PASS = "XXX"
 
 # Splunk config
 SPLUNK_ENABLED = True
-SPLUNK_HEC_URL = "http://10.10.10.10:8088/services/collector"
-SPLUNK_TOKEN = "XXX-XXX-XXX-XXX-XXX"
